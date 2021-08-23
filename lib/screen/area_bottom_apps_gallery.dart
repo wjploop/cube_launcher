199d1fbaa90963efdceba6398c6479462f934fa0
wjploop <wjploop@163.com>
2021-08-23 22:43:26 +0800

    删除不必要代码

--- lib/screen/area_bottom_apps_gallery.dart
+++ lib/screen/area_bottom_apps_gallery.dart
@@ -7,7 +7,6 @@ import 'package:cube_launcher/data/Repo.dart';
 import 'package:cube_launcher/data/event.dart';
 import 'package:cube_launcher/screen/area_top_bottom.dart';
 import 'package:event_bus/event_bus.dart';
-import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -101,42 +100,6 @@ class _AppGalleyState extends State<AppGalley> {
     var colorMap = faceColorMap.colorMap;
     var newMap = Map.of(colorMap)..[face] = color;
     faceColorMap.updateColor(newMap);
-  }
-
-  Future<bool> colorPickerDialog() async {
-    return ColorPicker(
-      color: context
-              .read<FaceMap>()
-              .colorMap[context.read<MenuState>().editFace] ??
-          Colors.yellow,
-      onColorChanged: (Color color) {
-        updateFaceColor(context.read<MenuState>().editFace, color);
-      },
-      width: 40,
-      height: 40,
-      borderRadius: 4,
-      spacing: 5,
-      runSpacing: 5,
-      wheelDiameter: 155,
-      heading: Text(
-        'Select color',
-        style: Theme.of(context).textTheme.subtitle1,
-      ),
-      actionButtons: ColorPickerActionButtons(
-          closeButton: true, dialogActionButtons: false),
-      pickersEnabled: const <ColorPickerType, bool>{
-        ColorPickerType.both: false,
-        ColorPickerType.primary: false,
-        ColorPickerType.accent: false,
-        ColorPickerType.bw: false,
-        ColorPickerType.custom: false,
-        ColorPickerType.wheel: true,
-      },
-      customColorSwatchesAndNames: colorsNameMap,
-    ).showPickerDialog(
-      context,
-      constraints: const BoxConstraints(minHeight: 320, minWidth: 300),
-    );
   }
 
   @override
@@ -273,21 +236,3 @@ class GalleryItem extends StatelessWidget {
 // Define some custom colors for the custom picker segment.
 // The 'guide' color values are from
 // https://material.io/design/color/the-color-system.html#color-theme-creation
-const Color guidePrimary = Color(0xFF6200EE);
-const Color guidePrimaryVariant = Color(0xFF3700B3);
-const Color guideSecondary = Color(0xFF03DAC6);
-const Color guideSecondaryVariant = Color(0xFF018786);
-const Color guideError = Color(0xFFB00020);
-const Color guideErrorDark = Color(0xFFCF6679);
-const Color blueBlues = Color(0xFF174378);
-// Make a custom ColorSwatch to name map from the above custom colors.
-final Map<ColorSwatch<Object>, String> colorsNameMap =
-    <ColorSwatch<Object>, String>{
-  ColorTools.createPrimarySwatch(guidePrimary): 'Guide Purple',
-  ColorTools.createPrimarySwatch(guidePrimaryVariant): 'Guide Purple Variant',
-  ColorTools.createAccentSwatch(guideSecondary): 'Guide Teal',
-  ColorTools.createAccentSwatch(guideSecondaryVariant): 'Guide Teal Variant',
-  ColorTools.createPrimarySwatch(guideError): 'Guide Error',
-  ColorTools.createPrimarySwatch(guideErrorDark): 'Guide Error Dark',
-  ColorTools.createPrimarySwatch(blueBlues): 'Blue blues',
-};
